d594193cafbf390fc61e30d45fc57e5616a3a1ef
Ankit Raj <ankitrajkumar1546024@gmail.com>
2025-04-22 10:45:31 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2025 Online Library Management System | Developed by : Ankit X Ankit </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
---
+++ library/captcha.php
@@ -0,0 +1,5 @@
+<?php 
+session_start(); 
+$text = 121; 
+$_SESSION["vercode"] = $text; 
+?>
